fe479d88dddd526210ff7cbe41301ff241fb5308
Barend <11941174+bvanrijn@users.noreply.github.com>
2019-12-16 20:14:46 +0100

    Show detailed terminal support error messages.
    imgshow now detects three sources of errors:
    
     - the user is not using iTerm2
     - the user is using Apple's default Terminal app
     - it is unclear whether the terminal app supports images
    
    See: #1

--- src/main.rs
+++ src/main.rs
@@ -73,23 +73,44 @@ impl InlineImageBuilder {
     }
 }
 
-fn is_iterm2() -> bool {
+enum UnsupportedReason {
+    /// Running inside Terminal.app is not supported
+    AppleTerminal,
+    /// $TERM_PROGRAM is not set
+    EnvNotSet,
+    /// Support for this terminal is unknown
+    Unknown,
+}
+
+fn is_supported_terminal() -> Result<(), UnsupportedReason> {
     let term_program_env = env::var("TERM_PROGRAM");
 
     if term_program_env.is_err() {
-        return false;
+        return Err(UnsupportedReason::EnvNotSet);
     }
 
-    let term_program = term_program_env.unwrap();
-
-    term_program == "iTerm.app"
+    match term_program_env.unwrap().as_ref() {
+        "iTerm.app" => Ok(()),
+        "Apple_Terminal" => Err(UnsupportedReason::AppleTerminal),
+        _ => Err(UnsupportedReason::Unknown),
+    }
 }
 
 fn main() {
-    if !is_iterm2() {
-        eprintln!("imgshow won't work in your terminal. Please use iTerm2: https://www.iterm2.com/index.html");
-
-        process::exit(1);
+    match is_supported_terminal() {
+        Ok(_) => (),
+        Err(UnsupportedReason::AppleTerminal) => {
+            eprintln!("Apple's Terminal application is not supported. Please use iTerm2 (https://www.iterm2.com) and try again.");
+            process::exit(1);
+        }
+        Err(UnsupportedReason::EnvNotSet) => {
+            eprintln!("The TERM_PROGRAM environment variable is not set.");
+            process::exit(2);
+        }
+        Err(UnsupportedReason::Unknown) => {
+            eprintln!("Your terminal is unsupported for an unknown reason. If your terminal supports images, please let me know by opening an issue.");
+            process::exit(3);
+        }
     }
 
     let mut image = InlineImageBuilder::new();
