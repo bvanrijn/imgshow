19a9d10bcb19fc9dc30e6805323a86fabf16558f
Barend <11941174+bvanrijn@users.noreply.github.com>
2019-12-16 21:23:08 +0100

    Move InlineImageBuilder to own file

---
+++ src/inline_image_builder.rs
@@ -0,0 +1,72 @@
+use base64::encode;
+use std::fmt;
+use std::fmt::Display;
+
+#[derive(Debug, Default)]
+pub struct InlineImageBuilder {
+    name: String,
+    size: usize,
+    width: String,
+    height: String,
+    preserve_aspect_ratio: bool,
+    inline: bool,
+    data: Vec<u8>,
+}
+
+impl InlineImageBuilder {
+    pub fn new() -> Self {
+        Self {
+            preserve_aspect_ratio: true,
+            ..Default::default()
+        }
+    }
+
+    pub fn set_data(&mut self, v: Vec<u8>) -> &mut Self {
+        self.data = v;
+        self
+    }
+
+    pub fn set_inline(&mut self, v: bool) -> &mut Self {
+        self.inline = v;
+        self
+    }
+}
+
+impl Display for InlineImageBuilder {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let esc = "\x1b";
+        let bel = "\x07";
+
+        write!(f, "{}]1337;File=", esc).expect("Can't write header");
+
+        if self.name != "" {
+            write!(f, "name={};", encode(&self.name)).expect("Can't write `name`");
+        }
+
+        if self.size != 0 {
+            write!(f, "size={};", self.size.to_string()).expect("Can't write `size`");
+        }
+
+        if self.width != "" {
+            write!(f, "width={};", self.width).expect("Can't write `width`");
+        }
+
+        if self.height != "" {
+            write!(f, "height={};", self.height).expect("Can't write `height`");
+        }
+
+        if self.preserve_aspect_ratio {
+            write!(f, "preserveAspectRatio=1;").expect("Can't write `preserveAspectRatio`");
+        } else {
+            write!(f, "preserveAspectRatio=0;").expect("Can't write `preserveAspectRatio`");
+        }
+
+        if self.inline {
+            write!(f, "inline=1;").expect("Can't write `inline`");
+        } else {
+            write!(f, "inline=0;").expect("Can't write `inline`");
+        }
+
+        write!(f, ":{}{}", encode(&self.data), bel)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,8 @@
-use base64::encode;
+mod inline_image_builder;
+
+use crate::inline_image_builder::InlineImageBuilder;
 use std::env;
-use std::fmt;
-use std::fmt::Display;
 use std::process;
-
-#[derive(Debug, Default)]
-struct InlineImageBuilder {
-    name: String,
-    size: usize,
-    width: String,
-    height: String,
-    preserve_aspect_ratio: bool,
-    inline: bool,
-    data: Vec<u8>,
-}
-
-impl Display for InlineImageBuilder {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let esc = "\x1b";
-        let bel = "\x07";
-
-        write!(f, "{}]1337;File=", esc).expect("Can't write header");
-
-        if self.name != "" {
-            write!(f, "name={};", encode(&self.name)).expect("Can't write `name`");
-        }
-
-        if self.size != 0 {
-            write!(f, "size={};", self.size.to_string()).expect("Can't write `size`");
-        }
-
-        if self.width != "" {
-            write!(f, "width={};", self.width).expect("Can't write `width`");
-        }
-
-        if self.height != "" {
-            write!(f, "height={};", self.height).expect("Can't write `height`");
-        }
-
-        if self.preserve_aspect_ratio {
-            write!(f, "preserveAspectRatio=1;").expect("Can't write `preserveAspectRatio`");
-        } else {
-            write!(f, "preserveAspectRatio=0;").expect("Can't write `preserveAspectRatio`");
-        }
-
-        if self.inline {
-            write!(f, "inline=1;").expect("Can't write `inline`");
-        } else {
-            write!(f, "inline=0;").expect("Can't write `inline`");
-        }
-
-        write!(f, ":{}{}", encode(&self.data), bel)
-    }
-}
-
-impl InlineImageBuilder {
-    fn new() -> Self {
-        Self {
-            preserve_aspect_ratio: true,
-            ..Default::default()
-        }
-    }
-
-    fn set_data(&mut self, v: Vec<u8>) -> &mut Self {
-        self.data = v;
-        self
-    }
-
-    fn set_inline(&mut self, v: bool) -> &mut Self {
-        self.inline = v;
-        self
-    }
-}
 
 enum UnsupportedReason {
     /// Running inside Terminal.app is not supported
